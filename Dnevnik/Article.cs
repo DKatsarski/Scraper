8aab64fb7bed00f20d98ac440b1f8d321fd828ed
DESKTOP-LT3V4GP\dkats <dd.katsarski@gmail.com>
2022-05-07 00:12:35 +0300

    added scraper for Dnevnik -> successfully downloading arcticles

---
+++ Dnevnik/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace Dnevnik
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Author { get; set; }
+    }
+}
---
+++ Dnevnik/Program.cs
@@ -0,0 +1,188 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dnevnik;
+using HtmlAgilityPack;
+using System.Text;
+using System.Text.RegularExpressions;
+
+//get all the dates for a period of time
+var startDate = DateTime.Parse("04/22/2022");
+var endDate = DateTime.Now;
+var allDatesFormatted = EachDay(startDate, endDate);
+var listOfAllDates = new Queue<string>(allDatesFormatted);
+var httpClient = new HttpClient();
+var httpDocument = new HtmlDocument();
+
+static IEnumerable<string> EachDay(DateTime from, DateTime thru)
+{
+    for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+        yield return day.ToString("yyyy/MM/dd");
+}
+
+
+while (listOfAllDates.Any())
+{
+    var date = listOfAllDates.Dequeue();
+    var linksOfTheDay = new Queue<string>(await TakeAllLinksOfDay(date));
+    var articleLink = linksOfTheDay.Dequeue();
+    var commentsOfCurrentArticle = linksOfTheDay.Where(x => x.Contains(articleLink)).FirstOrDefault();
+
+    Thread.Sleep(2000);
+    await ScrapeArticle(httpClient, httpDocument, articleLink);
+
+    if (commentsOfCurrentArticle != null)
+    {
+        Thread.Sleep(2000);
+        await ScrapeComments(httpClient, httpDocument, commentsOfCurrentArticle);
+    }
+
+
+
+}
+
+async Task ScrapeComments(HttpClient httpClient, HtmlDocument httpDocument, string commentsOfCurrentArticle)
+{
+
+}
+
+async Task ScrapeArticle(HttpClient httpClient, HtmlDocument htmlDocument, string link)
+{
+    var sb = new StringBuilder();
+    var html = await httpClient.GetStringAsync(link);
+    htmlDocument.LoadHtml(html);
+
+    var divs =
+    htmlDocument
+    .DocumentNode
+    .Descendants("article")
+    .Where(node => node.GetAttributeValue("class", "")
+    .Equals("general-article-v2 article"))
+    .ToList();
+
+    foreach (var div in divs)
+    {
+        var title = div
+            .Descendants("h1")
+            .FirstOrDefault()?
+            .InnerText;
+
+
+
+
+        var content = div
+        .SelectNodes("//div[@class='article-content']");
+
+        foreach (var node in content)
+        {
+            sb.AppendLine(String.Format("{0}, {1}, {2}", "2022/04/06", title, node.InnerText));
+        }
+
+        var filePath = @"C:\Users\\dkats\Desktop\asdff.csv";
+
+        var resultString = Regex.Replace(sb.ToString().Trim(), @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);
+        await File.AppendAllTextAsync(filePath, resultString, Encoding.UTF8);
+
+    }
+
+}
+
+var articles = await startCrawlerasync();
+
+/// <summary>
+/// article-content - class
+/// keywords - id
+
+/// </summary>
+
+static async Task<List<Article>> startCrawlerasync()
+{
+    var url = "https://www.dnevnik.bg/allnews/2022/04/25/";
+    var httpClient = new HttpClient();
+    var html = await httpClient.GetStringAsync(url);
+    var htmlDocument = new HtmlDocument();
+    htmlDocument.LoadHtml(html);
+    var articles = new List<Article>();
+    var list = new HashSet<string>();
+
+    var divs =
+        htmlDocument
+        .DocumentNode
+        .Descendants("div")
+        .Where(node => node.GetAttributeValue("class", "")
+        .Equals("grid-container"))
+        .ToList();
+
+
+    foreach (HtmlNode div in divs)
+    {
+        var article = new Article()
+        {
+            Content = div.Descendants("article").FirstOrDefault()?.InnerText
+        };
+
+        var a = div.Descendants("a")
+            .Select(node => node
+            .GetAttributeValue("href", String.Empty)).ToList();
+
+        list = div
+           .Descendants("a")
+           .Select(node => node
+           .GetAttributeValue("href", String.Empty))
+           .Where(x => x.StartsWith("http"))
+           .ToHashSet();
+
+
+        articles.Add(article);
+    }
+
+    return articles;
+}
+
+static async Task<HashSet<string>> TakeAllLinksOfDay(string dataInString)
+{
+    var url = "https://www.dnevnik.bg/allnews/" + dataInString;
+    var httpClient = new HttpClient();
+    var html = await httpClient.GetStringAsync(url);
+    var htmlDocument = new HtmlDocument();
+    htmlDocument.LoadHtml(html);
+    var listLinks = new HashSet<string>();
+
+    var divs =
+        htmlDocument
+        .DocumentNode
+        .Descendants("div")
+        .Where(node => node.GetAttributeValue("class", "")
+        .Equals("grid-container"))
+        .ToList();
+
+
+    foreach (HtmlNode div in divs)
+    {
+
+        var a = div.Descendants("a")
+            .Select(node => node
+            .GetAttributeValue("href", String.Empty)).ToList();
+
+        listLinks = div
+           .Descendants("a")
+           .Select(node => node
+           .GetAttributeValue("href", String.Empty))
+           .Where(x => x.StartsWith("http"))
+           .ToHashSet();
+    }
+
+    return listLinks;
+}
+
+
+
+
+static Func<int, HashSet<string>> FilterLinks(Func<int, HashSet<string>> links = null)
+{
+
+    return links;
+};
+
+
+
+
+
