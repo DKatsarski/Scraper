7e0efb0c16809ab9cadd5e0574f7a6a9944d7043
DESKTOP-LT3V4GP\dkats <dd.katsarski@gmail.com>
2022-05-07 01:16:14 +0300

    Added ef

---
+++ Dnevnik/Persistence/DnevnikContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnevnik.Persistence
+{
+    public class DnevnikContext : DbContext
+    {
+        public DnevnikContext()
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
